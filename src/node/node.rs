1ed30224487757690758d7a4f00670f5e29dc160
felixkamau <kamaufelix16@gmail.com>
2026-08-01 18:58:10 +0300

    feat: implemented the election timeout and election timer reset

--- src/node/node.rs
+++ src/node/node.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 use crate::{
     role::Role,
     state_module::{StateModule, state_module::LogEntry},
@@ -7,21 +9,45 @@ pub struct Node {
     id: u8,
     role: Role,
     state: StateModule,
+
+    election_deadline: Instant,
+    election_timeout: Duration,
+    // heartbeat_interval: Duration,
+    // heartbeat_deadline: Instant,
+}
+fn random_timeout() -> Duration {
+    let millis = rand::random_range(150..300);
+    let duration: Duration = Duration::from_millis(millis);
+    duration
 }
 
 impl Node {
     pub fn new(id: u8, peer_count: usize) -> Self {
+        let timeout = random_timeout();
         Node {
             id: id,
             role: Role::default(),
             state: StateModule::new(peer_count),
+            election_deadline: Instant::now() + timeout,
+            election_timeout: timeout,
         }
+    }
+
+    pub fn reset_election_timer(&mut self) {
+        self.election_timeout = random_timeout();
+        self.election_deadline = Instant::now() + self.election_timeout;
+    }
+
+    pub fn election_timed_out(&self) -> bool {
+        let timedout = Instant::now() >= self.election_deadline;
+        timedout
     }
 
     pub fn on_request_vote(&mut self, term: u8, candidate_id: u8) -> (u8, bool) {
         let (current_term, granted) = self.state.handle_request_vote(term, candidate_id);
         if granted {
             self.role = Role::FOLLOWER; // Fall back and reset time here
+            self.reset_election_timer();
         }
         (current_term, granted)
     }
@@ -44,10 +70,11 @@ impl Node {
             entries,
             leader_commit,
         );
-        if current_term > old_term {
+        if current_term > old_term && appended {
             // we heard from a vaild leader
             self.role = Role::FOLLOWER;
             // reset election timee here
+            self.reset_election_timer();
         }
         (current_term, appended)
     }
