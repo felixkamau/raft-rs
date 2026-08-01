ef710da7c7ef7e8af6ab4ce99c0ffcb009ea0634
felixkamau <kamaufelix16@gmail.com>
2026-08-01 18:57:07 +0300

    test: a simple node test, will be replaced later on

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 mod node;
-mod state_module;
 mod role;
+mod state_module;
 
 use crate::node::Node;
 fn main() {
-    let mut node = Node::new(1, 2);
+    // let mut node = Node::new(1, 2);
     // node.on_request_vote();
+    let mut nodes: Vec<Node> = (1..=3).map(|id| Node::new(id, 2)).collect();
+    // Node 1 requests a vote from Node 2
+    let (term, granted) = nodes[1].on_request_vote(1, 1);
+
+    println!("Term: {}, Vote granted: {}", term, granted);
 }
